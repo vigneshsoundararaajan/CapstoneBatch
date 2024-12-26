4dad0bfd837c9d39320f7dda95f81c3689983141
Vignesh Soundararajan <vigneshsviolet@gmail.com>
2024-12-26 22:12:29 +0530

    Version 1.1

--- src/main/java/com/example/product_service/ProductServiceApplication.java
+++ src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
